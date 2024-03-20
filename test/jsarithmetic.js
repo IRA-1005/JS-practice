f4af7c185743ffb8c80b4d73121fee505d74642f
IRA-1005 <ivan.cirilo.ramos@gmail.com>
2024-03-20 16:51:14 +0100

    complete JS exercise 01

---
+++ test/jsarithmetic.js
@@ -0,0 +1,33 @@
+/*
+    let math;
+    math = 100 + 5;
+    let math_letters
+        , a = 5
+        , b = 3;
+    math_letters = (a ** b)
+    console.log (typeof a)
+    a = 6;
+    console.log(math, math_letters,)
+/*
+
+/* OP exercises 
+console.log(23 + 97);
+console.log(23 + 97 + 1 + 2 + 4 + 5 + 6);
+console.log((6 + 4 + 9) / 77);
+
+let a = 10;
+(9 * a);
+let b = 7 * a;
+console.log(b)
+let MAX = 57;
+let actual = (MAX - 13);
+let percentage = (actual / MAX)
+console.log(percentage);*/
+
+const string = "The Revolution will not be televised.";
+const badString = string;
+console.log(string, badString);
+const single = 'Single quotes';
+const double = "double quotes";
+const backtick = `backtick`;
+console.log (single, double, backtick);
