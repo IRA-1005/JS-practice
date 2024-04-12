4fa4d1e7d4db6ace2de4ded7833678ef65949359
IRA-1005 <ivan.cirilo.ramos@gmail.com>
2024-04-12 13:38:26 +0200

    complete Data Types and contitionals

--- test/jsarithmetic.js
+++ test/jsarithmetic.js
@@ -24,10 +24,39 @@ let actual = (MAX - 13);
 let percentage = (actual / MAX)
 console.log(percentage);*/
 
-const string = "The Revolution will not be televised.";
+/*const string = "The Revolution will not be televised.";
 const badString = string;
 console.log(string, badString);
 const single = 'Single quotes';
 const double = "double quotes";
 const backtick = `backtick`;
 console.log (single, double, backtick);
+let age = 51
+if (age > 13 || age < 91)
+    alert ("legal");
+let age2 = 13
+if (!(age2 >=14 && age2<=90))
+    alert ("ill eagle");*/
+
+let username = prompt("Who's there", "");
+if (username === "" || username === null) {
+    console.log("canceled");
+}
+else if (username.toLowerCase() === "admin") {
+        let pass = prompt("password?", "");
+            if (pass === "TheMaster")
+            {
+                alert("Welcome!");
+            }
+            else if (pass === "" || null) {
+            console.log ("password not input");
+            }
+                else {
+                    alert("wrong password");
+                }
+    }
+        else 
+        {
+            console.log ("I dont know you")
+        } 
+
